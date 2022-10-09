4a623f1ce467cd3922236349a970038b58b0ecea
Ricardo Costa <ricardo.costa@outsystems.com>
2022-10-09 13:14:00 +0100

    initial commit after generating the project with spring initalizr

---
+++ superduperdrive/src/test/java/com/rmmcosta/superduperdrive/SuperduperdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmmcosta.superduperdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperduperdriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
