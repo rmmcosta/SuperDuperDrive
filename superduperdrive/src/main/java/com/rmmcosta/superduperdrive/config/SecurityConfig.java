b40bfae3c5af68f3a8e84027b1cd0362d8c4e759
Ricardo Costa <ricardo.costa@outsystems.com>
2022-10-09 16:41:53 +0100

    Login and Signup templates

---
+++ superduperdrive/src/main/java/com/rmmcosta/superduperdrive/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.rmmcosta.superduperdrive.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((authz) -> authz
+                        .antMatchers("/signup", "/css/**", "/js/**", "/images/**", "/favicon.ico", "/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        httpSecurity
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
+        return httpSecurity.build();
+    }
+}
